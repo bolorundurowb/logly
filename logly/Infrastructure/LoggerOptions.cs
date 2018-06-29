56664fd9c49d53b284a52f84b3b411d1a12925f7
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2018-06-28 21:59:05 -0600

    feature: add logger options

---
+++ logly/Infrastructure/LoggerOptions.cs
@@ -0,0 +1,30 @@
+﻿namespace logly.Infrastructure
+{
+    public class LoggerOptions
+    {
+        /// <summary>
+        /// Whether or not the request method should be logged
+        /// </summary>
+        public bool ShowMethod { get; set; }
+
+        /// <summary>
+        /// Whether or not the request Url should be logged
+        /// </summary>
+        public bool ShowUrl { get; set; }
+
+        /// <summary>
+        /// Whether or not the response status code should be logged
+        /// </summary>
+        public bool ShowStatusCode { get; set; }
+
+        /// <summary>
+        /// Whether or not the request Url should be logged
+        /// </summary>
+        public bool ShowResponseLength { get; set; }
+
+        /// <summary>
+        /// Whether or not the amount of time taken for the response should be logged
+        /// </summary>
+        public bool ShowResponseTime { get; set; }
+    }
+}
