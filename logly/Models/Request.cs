9936953d097e87709ca2c0e54ab947fb2cc5c465
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2018-06-30 08:21:19 -0700

    feature: add data models

---
+++ logly/Models/Request.cs
@@ -0,0 +1,17 @@
+﻿namespace logly.Models
+{
+    internal class Request
+    {
+        public RequestType Type { get; set; }
+        public string Url { get; set; }
+    }
+    
+    internal enum RequestType
+    {
+        GET,
+        POST,
+        PUT,
+        DELETE,
+        PATCH
+    }
+}
---
+++ logly/Models/Response.cs
@@ -0,0 +1,9 @@
+﻿namespace logly.Models
+{
+    internal class Response
+    {
+        public int StatusScode { get; set; }
+        public int ResponseLength { get; set; }
+        public double ResponseTime { get; set; }
+    }
+}
